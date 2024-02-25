6be71f86424811102c73fa2fa95d9e9e9f3b2e2d
Leonardo Vergara R. <leovergara@gmail.com>
2024-02-25 10:22:14 -0300

    nodeJS puppeteer last version

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-import puppeteer from "puppeteer";
+ import puppeteer from "puppeteer";
 import fs from 'fs/promises';
 
 ( async () => {
         const navegador = await puppeteer.launch({
-             headless: false, //'new',
-             slowMo : 300,
+             //headless: false, //'new',
+             slowMo : 200,
              //
         })
         const pagina = await navegador.newPage()
         await pagina.goto('https://onefootball.com/es/partido/2404687')
-        // await pagina.click('#onetrust-accept-btn-handler')
-        // await new Promise((resolve) => setTimeout(resolve, 3000));
-        
-        
+        await pagina.screenshot({path: 'capturaPantalla.png'});
+
         const result = await pagina.evaluate(() => {
            let goles =  document.querySelector('.MatchScore_data__ahxqz').innerText
 
            
            return {
 
-            goles,
+            goles : goles.toString().replace('\n:\n', '-'), 
+
             }
         })
 
