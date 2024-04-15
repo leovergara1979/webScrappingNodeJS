67a922ef2203b9f793b44eccb8406185fa30d887
Leonardo Vergara R. <leovergara@gmail.com>
2024-04-15 13:10:56 -0400

    click en boton y captura de pantalla + resultado con minutos

--- index.js
+++ index.js
@@ -1,31 +1,84 @@
- import puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
 import fs from 'fs/promises';
 
+
+
+
+//*************************************************************solo marcador de goles********************************
+
 ( async () => {
-        const navegador = await puppeteer.launch({
-             //headless: false, //'new',
-             slowMo : 200,
-             //
-        })
-        const pagina = await navegador.newPage()
-        await pagina.goto('https://onefootball.com/es/partido/2404687')
-        await pagina.screenshot({path: 'capturaPantalla.png'});
+    const navegador = await puppeteer.launch({
+         //headless: false, //'new',
+         //slowMo : 200,
+    })
+    const pagina = await navegador.newPage()
+    await pagina.goto('https://onefootball.com/es/partido/2398356')
+    
+    const element = await pagina.waitForSelector('#onetrust-accept-btn-handler');
+    await element.click(); 
+                           
+    
+    const result = await pagina.evaluate(() => {
+        // let goles =  document.querySelector('.MatchScore_scores__Hnn5f').innerText
+        let goles =  document.querySelector('.MatchScore_data__ahxqz').innerText
 
-        const result = await pagina.evaluate(() => {
-           let goles =  document.querySelector('.MatchScore_data__ahxqz').innerText
-
-           
-           return {
-
+        
+        return {
+            
             goles : goles.toString().replace('\n:\n', '-'), 
+            
+            
+        }
+    })
+    
+    console.log(result)
+    
+    await fs.writeFile('salida.json', JSON.stringify(result, null, 2))
+    //await pagina.screenshot({path: 'capturaPantalla1.png'});
+    setTimeout(function(){
+        pagina.screenshot({path: 'capturaPantalla.png'});
+    }, 1500);
 
-            }
-        })
+    setTimeout(function(){
+        navegador.close()
+    }, 2000);
+    
+})() 
 
-        console.log(result)
 
-        await fs.writeFile('salida.json', JSON.stringify(result, null, 2))
-        await navegador.close()
-    })()        
+
+
+
+//*************************************************************old funcionando********************************
+
+// ( async () => {
+//         const navegador = await puppeteer.launch({
+//              headless: false, //'new',
+//              slowMo : 200,
+//         })
+//         const pagina = await navegador.newPage()
+//         await pagina.goto('https://onefootball.com/es/partido/2424517')
+        
+//         //const element = await pagina.waitForSelector('#onetrust-accept-btn-handler');
+//         //await element.click(); 
+//         await pagina.click('#onetrust-accept-btn-handler')
+        
+//         const result = await pagina.evaluate(() => {
+//             let goles =  document.querySelector('.MatchScore_data__ahxqz').innerText
+            
+//             return {
+                
+//                 goles : goles.toString().replace('\n:\n', '-'), 
+                
+                
+//             }
+//         })
+        
+//         console.log(result)
+        
+//         await fs.writeFile('salida.json', JSON.stringify(result, null, 2))
+//         await pagina.screenshot({path: 'capturaPantalla.png'});
+//         await navegador.close()
+//     })()        
 
     
